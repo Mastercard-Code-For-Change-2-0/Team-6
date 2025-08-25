4b7343ca11ad8488a2c146035870ff3dee1b99ca
Yuvraj <226978013+imyuvi1812@users.noreply.github.com>
2025-08-25 11:29:32 +0530

    controller pushed

---
+++ controllers/studentdatacontroller.js
@@ -0,0 +1,237 @@
+import {deleteOne, getOne, updateOne} from "./handlerFactory.js";
+import Student from "../models/studentModel.js";
+import catchAsync from "../utils/CatchAsync.js";
+import AppError from "../utils/appError.js";
+
+const getStudent = getOne(Student);
+const updateStudent = updateOne(Student);
+const deleteStudent = deleteOne(Student);
+
+const createOneStudent = catchAsync(async (req, res, next) => {
+    const {organization} = req.params;
+    const {registrationNumber, satsNumber} = req.body;
+
+    let query;
+    if (organization === 'college') {
+        query = {
+            registrationNumber
+        }
+    } else {
+        query = {
+            satsNumber
+        }
+    }
+
+    const user = await Student.findOne(query);
+
+    if (user) {
+        return res.status(500).json({
+            error: 'duplicate',
+            message: 'A student with this Registration/SATS number already exists!',
+        });
+    }
+
+    const newDoc = await Student.create({
+        ...req.body,
+        organization: organization.toUpperCase(),
+    });
+
+    res.status(201).json({
+        status: "success",
+        data: {newDoc}
+    });
+});
+
+const getStudents = catchAsync(async (req, res) => {
+    const {name, ...otherQueryParams} = req.query;
+    const query = {
+        ...otherQueryParams,
+        organization: req.params.organization.toUpperCase(),
+    };
+
+    if (name) {
+        query.name = {$regex: name, $options: "i"};
+    }
+
+    const students = await Student.find(query);
+
+    let males = 0, females = 0;
+    for (const student of students) {
+        switch (student.gender) {
+            case "male":
+                males++;
+                break;
+            case "female":
+                females++;
+                break;
+        }
+    }
+
+    res.status(200).json({
+        status: 'success',
+        length: students.length,
+        data: {
+            stats: {males, females},
+            students
+        }
+    });
+});
+
+const updateStudentsFee = catchAsync(async (req, res) => {
+    const {fixedFee} = req.body;
+    const {organization} = req.params;
+
+    await Student.updateMany({
+        organization: organization.toUpperCase(),
+        ...req.query
+    }, {
+        fixedFee: fixedFee
+    });
+
+    res.status(200).json({
+        status: 'success',
+    });
+});
+
+const searchStudents = catchAsync(async (req, res) => {
+    const {organization, searchQuery} = req.params;
+    const {course, year} = req.query;
+
+    if (!organization || !searchQuery) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Organization and search query are required',
+        });
+    }
+    if (!['college', 'school'].includes(organization.toLowerCase())) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Invalid organization type. Must be "college" or "school"',
+        });
+    }
+
+    const query = [
+        {name: {$regex: searchQuery, $options: "i"}},
+        {gender: {$regex: searchQuery, $options: "i"}},
+        {phoneNumber: {$regex: searchQuery, $options: "i"}},
+        {phoneNumber2: {$regex: searchQuery, $options: "i"}},
+    ];
+
+    let organizationParams;
+    if (organization === 'college') {
+        organizationParams = [
+            {course: {$regex: searchQuery, $options: "i"}},
+            {registrationNumber: {$regex: searchQuery, $options: "i"}}
+        ];
+    } else {
+        organizationParams = [
+            {class: {$regex: searchQuery, $options: "i"}},
+            {satsNumber: {$regex: searchQuery, $options: "i"}}
+        ];
+    }
+
+    query.push(...organizationParams);
+
+    const andQuery = [
+        {organization: organization.toUpperCase()},
+        {
+            $or: query
+        }
+    ];
+
+    if (course) {
+        if (organization === 'college') {
+            andQuery.push({course});
+        } else {
+            andQuery.push({class: course});
+        }
+    }
+
+    if (year) andQuery.push({year});
+
+    const students = await Student.find({
+        $and: andQuery
+    });
+
+
+    res.status(200).json({
+        status: 'success',
+        length: students.length,
+        data: {
+            students
+        }
+    });
+});
+
+const getStudentsYears = catchAsync(async (req, res) => {
+    const {organization} = req.params;
+    const years = await Student.find({...req.query, organization: organization.toUpperCase()}).select('expectedYearOfPassing');
+    res.status(200).json({
+        status: 'success',
+        data: {
+            years
+        }
+    });
+});
+
+const promoteStudents = catchAsync(async (req, res) => {
+    const {course, year} = req.query;
+    const {organization} = req.params;
+
+    if (!course || !year || !organization) {
+        return res.status(400).json({
+            status: 'error',
+            message: 'Please provide course, year and organization."',
+        });
+    }
+
+    let newYear;
+    switch (year) {
+        case 'newAdmission':
+            newYear = '1';
+            break;
+        case '1':
+            newYear = '2';
+            break;
+        case '2':
+            newYear = '3';
+            break;
+        case '3':
+            newYear = '4';
+            break;
+        case '4':
+            newYear = 'passedOut';
+            break;
+    }
+
+   
+
+    await Student.updateMany({
+        course,
+        year,
+        organization: organization.toUpperCase(),
+    },
+    [
+        {
+            $set: {
+                previousFee: {
+                    $add: [
+                        "$previousFee",
+                        {
+                            $max: [0, { $subtract: ["$fixedFee", "$paidFee"] }]
+                        }
+                    ]
+                },
+                paidFee: 0,
+                year: newYear
+            }
+        }
+    ]);
+
+    res.status(200).json({
+        status: 'success',
+        message: 'Students promoted successfully.',
+    });
+});
+
+export {createOneStudent, updateStudent, getStudent, deleteStudent, getStudents, updateStudentsFee, searchStudents, getStudentsYears, promoteStudents};
---
+++ models/studentModel.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    organization: {
+        type: String,
+        enum: ['SCHOOL'],
+        required: true,
+    },
+    rollNumber: String,
+    satsNumber: String,
+    registrationNumber: String,
+    class: String,
+    section: Number,
+    course: String,
+    durationInYear: {
+        type: Number
+    },
+    batch: {
+        type: String,
+    },
+    year: {
+        type: String
+    },
+    
+    dateOfBirth: {
+        type: Date,
+    },
+    adhaarNumber: String,
+    voterNumber: String,
+    passportNumber: String,
+    dateOfAdmission: {
+        type: Date,
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female"],
+    },
+    presentAddress: String,
+    permanentAddress: String,
+    city: String,
+    state: String,
+    pinCode: String,
+    religion: String,
+    caste: String,
+    subCaste: String,
+    category: {
+        type: String,
+        // enum: ["general", "obc", "sc", "st"],
+    },
+    categoryCertificateEnclosed: {
+        type: String,
+        enum: ["yes", "no"],
+    },
+    parentsIncome: String,
+    inComeCertificateEnclosed: {
+        type: String,
+        enum: ["yes", "no"],
+    },
+    fixedFee: {
+        type: Number,
+        default: 0,
+    },
+    paidFee: {
+        type: Number,
+        default: 0,
+    },
+    previousFee: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    }
+});
+
+const Student = mongoose.model("Student", studentSchema);
